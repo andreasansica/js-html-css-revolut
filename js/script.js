adeaaac0e0ad905203c7b0daff0d9434a69ede20
Andrea Sansica <sansica.andrea99@gmail.com>
2020-11-05 09:41:28 +0100

    ok

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@
 
 $(document).ready(function () {
 
-
+// click navbar menu
   $('li.dropitem').click(function () {
 
+    var block = $(".dblock")
 
-
-    if ($(this).children(".dropdown").hasClass("dblock")) {
-     $(".dblock").removeClass("dblock");
+    if ($(this).find(".dropdown").hasClass("dblock")) {
+     block.removeClass("dblock");
    } else {
-     $(".dblock").removeClass("dblock");
-     $(this).children(".dropdown").addClass("dblock");
+     block.removeClass("dblock");
+     $(this).find(".dropdown").addClass("dblock");
    }
    });
 
